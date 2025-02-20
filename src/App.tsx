e193d340d2531f9ba8ea0069e88a67ae0498d05f
sowmik-sec <sowmik.sec@gmail.com>
2025-02-20 22:31:07 +0600

    show box if it is the modulus of 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import {
-  decrement,
-  increment,
-  incrementByValue,
-} from "./redux/features/counterSlice";
+import { decrement, increment } from "./redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
   const count = useAppSelector((state) => state.counter.count);
   const dispatch = useAppDispatch();
+  const n = Array.from({ length: count / 5 }, (_, i) => i + 1);
+  console.log(n);
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="flex border border-purple-300 rounded-md p-10">
-        <button
-          onClick={() => dispatch(increment())}
-          className="px-3 py-2 cursor-pointer rounded-md bg-green-500 text-xl font-semibold text-white"
-        >
-          Increment
-        </button>
-        <button
+      <div className="">
+        <div className="flex border border-purple-300 rounded-md p-10">
+          <button
+            onClick={() => dispatch(increment())}
+            className="px-3 py-2 cursor-pointer rounded-md bg-green-500 text-xl font-semibold text-white"
+          >
+            Increment
+          </button>
+          {/* <button
           onClick={() => dispatch(incrementByValue(5))}
           className="px-3 py-2 cursor-pointer rounded-md bg-green-500 text-xl font-semibold text-white"
         >
           Increment by 5
-        </button>
-        <h1 className="text-3xl mx-10">{count}</h1>
-        <button
-          onClick={() => dispatch(decrement())}
-          className="px-3 py-2 rounded-md cursor-pointer bg-green-500 text-xl font-semibold text-white"
-        >
-          Decrement
-        </button>
+        </button> */}
+          <h1 className="text-3xl mx-10">{count}</h1>
+          <button
+            onClick={() => dispatch(decrement())}
+            className="px-3 py-2 rounded-md cursor-pointer bg-green-500 text-xl font-semibold text-white"
+          >
+            Decrement
+          </button>
+        </div>
+        <div className="flex gap-6 mt-3">
+          {" "}
+          {n.map((_, i) => (
+            <div key={i} className="p-5 bg-amber-500 rounded-md"></div>
+          ))}
+        </div>
       </div>
     </div>
   );
