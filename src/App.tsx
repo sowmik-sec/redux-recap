dc45111dabd7fb0d586e1b65b66f3b1340bf1195
sowmik-sec <sowmik.sec@gmail.com>
2025-02-20 21:29:47 +0600

    Initialize store and slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 function App() {
-
   return (
-    <>
-      <h2 className="text-3xl">Hello world</h2>
-    </>
-  )
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-300 rounded-md p-10">
+        <button className="px-3 py-2 cursor-pointer rounded-md bg-green-500 text-xl font-semibold text-white">
+          Increment
+        </button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="px-3 py-2 rounded-md cursor-pointer bg-green-500 text-xl font-semibold text-white">
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
